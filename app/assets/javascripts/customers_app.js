2a081b9d2c353d59e1b4e292da5925f27c3ec606
Zhumaniyazov Kairat <k.zhumaniyazov@gmail.com>
2017-06-28 02:58:57 +0600

    + nested controller

--- app/assets/javascripts/customers_app.js
+++ app/assets/javascripts/customers_app.js
@@ -64,10 +64,10 @@ app.controller('CustomerSearchController', [
 app.controller('CustomerDetailController', [
   '$scope', '$routeParams', '$resource',
   function($scope, $routeParams, $resource) {
-    var customerId = $routeParams.id;
+    $scope.customerId = $routeParams.id;
     var Customer = $resource('/customers/:customerId.json')
 
-    $scope.customer = Customer.get({ 'customerId': customerId });
+    $scope.customer = Customer.get({ 'customerId': $scope.customerId });
     alert('ajax call initiated!');
   }
 ]);
@@ -76,6 +76,9 @@ app.controller('CustomerCreditCardController', [
   '$scope', '$resource',
   function($scope, $resource) {
     var CreditCardInfo = $resource('/fake_billing.json');
-    $scope.creditCard = CreditCardInfo.get({ 'cardholder_id': 1234 });
+
+    $scope.setCardholderId = function(cardholderId) {
+      $scope.creditCard = CreditCardInfo.get({ 'cardholder_id': cardholderId });
+    }
   }
 ]);
