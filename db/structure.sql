1e51835cab5e20af8102d04e85b2df231ba9b6c8
Zhumaniyazov Kairat <k.zhumaniyazov@gmail.com>
2017-06-28 00:46:40 +0600

    + first materialized view

--- db/structure.sql
+++ db/structure.sql
@@ -105,6 +105,59 @@ CREATE TABLE customers_billing_addresses (
 
 
 --
+-- Name: customers_shipping_addresses; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE customers_shipping_addresses (
+    id integer NOT NULL,
+    customer_id integer NOT NULL,
+    address_id integer NOT NULL,
+    "primary" boolean DEFAULT false NOT NULL
+);
+
+
+--
+-- Name: states; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE states (
+    id integer NOT NULL,
+    code character varying NOT NULL,
+    name character varying NOT NULL
+);
+
+
+--
+-- Name: customer_details; Type: VIEW; Schema: public; Owner: -
+--
+
+CREATE VIEW customer_details AS
+ SELECT customers.id AS customer_id,
+    customers.first_name,
+    customers.last_name,
+    customers.email,
+    customers.username,
+    customers.created_at AS joined_at,
+    billing_address.id AS billing_address_id,
+    billing_address.street AS billing_street,
+    billing_address.city AS billing_city,
+    billing_state.code AS billing_state,
+    billing_address.zipcode AS billing_zipcode,
+    shipping_address.id AS shipping_address_id,
+    shipping_address.street AS shipping_street,
+    shipping_address.city AS shipping_city,
+    shipping_state.code AS shipping_state,
+    shipping_address.zipcode AS shipping_zipcode
+   FROM ((((((customers
+     JOIN customers_billing_addresses ON ((customers.id = customers_billing_addresses.customer_id)))
+     JOIN addresses billing_address ON ((billing_address.id = customers_billing_addresses.address_id)))
+     JOIN states billing_state ON ((billing_address.state_id = billing_state.id)))
+     JOIN customers_shipping_addresses ON (((customers.id = customers_shipping_addresses.customer_id) AND (customers_shipping_addresses."primary" = true))))
+     JOIN addresses shipping_address ON ((shipping_address.id = customers_shipping_addresses.address_id)))
+     JOIN states shipping_state ON ((shipping_address.state_id = shipping_state.id)));
+
+
+--
 -- Name: customers_billing_addresses_id_seq; Type: SEQUENCE; Schema: public; Owner: -
 --
 
@@ -143,18 +196,6 @@ ALTER SEQUENCE customers_id_seq OWNED BY customers.id;
 
 
 --
--- Name: customers_shipping_addresses; Type: TABLE; Schema: public; Owner: -
---
-
-CREATE TABLE customers_shipping_addresses (
-    id integer NOT NULL,
-    customer_id integer NOT NULL,
-    address_id integer NOT NULL,
-    "primary" boolean DEFAULT false NOT NULL
-);
-
-
---
 -- Name: customers_shipping_addresses_id_seq; Type: SEQUENCE; Schema: public; Owner: -
 --
 
@@ -179,17 +220,6 @@ ALTER SEQUENCE customers_shipping_addresses_id_seq OWNED BY customers_shipping_a
 
 CREATE TABLE schema_migrations (
     version character varying NOT NULL
-);
-
-
---
--- Name: states; Type: TABLE; Schema: public; Owner: -
---
-
-CREATE TABLE states (
-    id integer NOT NULL,
-    code character varying NOT NULL,
-    name character varying NOT NULL
 );
 
 
@@ -454,6 +484,7 @@ INSERT INTO "schema_migrations" (version) VALUES
 ('20170626111028'),
 ('20170626114455'),
 ('20170626141821'),
-('20170627133410');
+('20170627133410'),
+('20170627174105');
 
 
