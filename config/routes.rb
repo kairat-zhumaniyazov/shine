11d2eb10e1074ceddfaf8403a0afe69b784bb86c
Zhumaniyazov Kairat <k.zhumaniyazov@gmail.com>
2017-06-27 18:33:11 +0600

    + angular customer details controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'dashboard#index'
 
-  resources :customers, only: :index
+  resources :customers, only: [:index, :show]
 
   get 'angular_test', to: 'angular_test#index'
 end
