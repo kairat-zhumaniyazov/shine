deda815414426cbe10030cabecfdfed9e8f3566a
Zhumaniyazov Kairat <k.zhumaniyazov@gmail.com>
2017-06-27 16:28:47 +0600

    + customers.json response spec

--- spec/javascripts/customers_app/controllers/customer_search_controller_spec.js
+++ spec/javascripts/customers_app/controllers/customer_search_controller_spec.js
@@ -18,4 +18,48 @@ describe('CustomerSearchController', function() {
     });
 
   });
+
+  describe('Fetching Search Results', function() {
+    var scope = null,
+        controller = null,
+        httpBackend = null,
+        serverResults = [
+          {
+            id: 123,
+            first_name: 'Bob',
+            last_name: 'Jones',
+            email: 'bjones@foo.net',
+            username: 'jonesy'
+          },
+          {
+            id: 456,
+            first_name: 'Bob',
+            last_name: 'Jognsons',
+            email: 'johnboy@bar.info',
+            username: 'bobbyj'
+          }
+        ];
+
+    beforeEach(module('customers'));
+
+    beforeEach(inject(function($controller, $rootScope, $httpBackend) {
+      scope = $rootScope.$new();
+      httpBackend = $httpBackend;
+      controller = $controller('CustomerSearchController', {
+        $scope: scope
+      });
+    }));
+
+    beforeEach(function() {
+      httpBackend.when('GET', '/customers.json?keywords=bob&page=0').respond(serverResults);
+    });
+
+    // ----
+
+    it('populates the customer list with the results', function() {
+      scope.search('bob');
+      httpBackend.flush();
+      expect(scope.customers).toEqualData(serverResults);
+    });
+  });
 });
